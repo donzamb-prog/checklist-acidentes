49d5c60096380cef4fa4fb1c06b3e3c5d9827bfd
donzamb-prog <donzambeli@gmail.com>
2025-12-26 21:49:47 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,25 @@
-const CACHE_NAME = 'checklist-acidentes-v2';
+const CACHE_NAME = 'checklist-acidentes-gh-v1';
+
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/service-worker.js',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  '/checklist-acidentes/',
+  '/checklist-acidentes/index.html',
+  '/checklist-acidentes/manifest.json',
+  '/checklist-acidentes/service-worker.js',
+  '/checklist-acidentes/icons/icon-192.png',
+  '/checklist-acidentes/icons/icon-512.png'
 ];
+
 self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k !== CACHE_NAME ? caches.delete(k) : null)))
     )
   );
   self.clients.claim();
@@ -23,6 +27,6 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
+    caches.match(event.request).then(cached => cached || fetch(event.request))
   );
 });
